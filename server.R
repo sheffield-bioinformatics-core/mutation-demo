2340dfc148ac90d0649c22e79e9c216c984a5beb
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-17 11:40:39 +0100

    Removed dependency on bulky BSgenome.Hsapiens.UCSC.hg19 package, instead using pre-extracted BRCA1 sequence.

--- server.R
+++ server.R
@@ -1,44 +1,32 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-
 library(RColorBrewer)
+library(ggplot2)
+library(gridExtra)
 
 mypal <- brewer.pal(9,"Set1")
 names(mypal) <- c("red","blue","green","purple","orange","yellow","brown","pink","grey")
 
+cat("Loading BRCA1 sequence\n")
+load('brca1seq.RData')
+
 shinyServer(function(input, output) {
-  
-  
+
   generate.sequence <- reactive({
-    library(org.Hs.eg.db)
-    library(BSgenome.Hsapiens.UCSC.hg19)
-    library(ggplot2)
-    library(gridExtra)
-    hg19 <- BSgenome.Hsapiens.UCSC.hg19
-    
-    start <- sample(41196312:41277500,1)
-    length <- input$length  - 1
-    
-    seq <-strsplit(as.character(getSeq(hg19, "chr17",start,start+length)),"")[[1]]
-    seq
+    length <- input$length
+    start <- sample(1:(nchar(brca1seq) - length + 1), 1)
+    seq <- substring(brca1seq, start, start + length - 1)
+    seq <- strsplit(seq, "")[[1]]
+
     out <- NULL
     out[[1]] <- seq
-    
+
     df <- data.frame(pos=1:length(seq),letter=seq)
-    nPerms <- input$choices - 1 
+    nPerms <- input$choices - 1
     seqList <- NULL
     diffList <- NULL
     seqList[[1]] <- seq
     diffList[[1]] <- 0
-    
-    maxChanges <- length(seq)*0.2
-      
+
     for(i in 1:nPerms){
       #pick at random how many positions to change
       posToChange <- sample(1:length(seq),sample(1:4,1))
@@ -48,112 +36,112 @@ shinyServer(function(input, output) {
       seqList[[i+1]] <- seq2
       diffList[[i+1]] <- sum(seq != seq2)
     }
-    
+
     df$Number <- rep(sample(1:(nPerms+1)),each=length(seq))
     df$Patient <- paste0("Patient",df$Number)
     df$Mutated <- do.call(c, lapply(seqList, function(x) x != seq))
-    
+
     out[[2]] <- df
     out[[3]] <- seqList
     out[[4]] <- diffList
     out
-    
+
   }
   )
-  
-  
-  
-  output$distPlot <- reactivePlot(function(){
+
+
+
+  output$distPlot <- renderPlot({
     ##Seems like I have to define these defaults?
     start <- 41196312
     length<-10
     nPerms  <- 5
     seq <- generate.sequence()[[1]]
-    
+
     df <- data.frame(pos=1:length(seq),letter=seq,Patient="Human Genome")
     df$letter <- factor(df$letter, levels= c("A","C","G","T"))
     useColour <- ifelse(input$useColour == "Yes",TRUE,FALSE)
     useText <- ifelse(input$useText == "Yes",TRUE,FALSE)
-    
+
     if(useColour){
-    gg <- ggplot(df, aes(x=pos,y=1,fill=letter,label=letter)) +geom_tile(position="identity") 
+    gg <- ggplot(df, aes(x=pos,y=1,fill=letter,label=letter)) +geom_tile(position="identity")
     } else gg <- ggplot(df, aes(x=pos,y=1,label=letter)) +geom_tile(position="identity",fill="white")
-    
-    
-    gg <- gg + scale_fill_manual(values=c("A" = as.character(mypal[3]),"C"=as.character(mypal[2]),"G"=as.character(mypal[6]),"T"=as.character(mypal[1])))
+
+
+    gg <- gg + scale_fill_manual(values=c("A" = as.character(mypal[3]),"C"=as.character(mypal[2]),"G"=as.character(mypal[6]),"T"=as.character(mypal[1])), drop = FALSE)
     gg <- gg + facet_wrap(~Patient)
-    
+
     if(useText) gg <- gg + geom_text()
-    
-    gg <- gg + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) 
+
+    gg <- gg + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) + theme(legend.position = "top", legend.title = element_blank())
     breaks <- data.frame(xs = c(0.5, 1:(length(seq))+0.5))
     gg <- gg + geom_vline(data=breaks, aes(xintercept=xs)) + xlab("")
-    
+
     gg
-    
-    
+
+
     df <- generate.sequence()[[2]]
     df$letter <- factor(df$letter, levels= c("A","C","G","T"))
-    
+
     if(useColour){
-      gg2 <- ggplot(df, aes(x=pos,y=1,fill=letter,label=letter)) +geom_tile() 
+      gg2 <- ggplot(df, aes(x=pos,y=1,fill=letter,label=letter)) +geom_tile()
     } else   gg2 <- ggplot(df, aes(x=pos,y=1,label=letter)) +geom_tile(fill="white")
-    
+
     if(useText) gg2 <- gg2 + geom_text()
-    
+
     gg2 <- gg2 + scale_fill_manual(values=c("A" = as.character(mypal[3]),"C"=as.character(mypal[2]),"G"=as.character(mypal[6]),"T"=as.character(mypal[1]))) + ggtitle("Which patients show a mutation?")
     gg2 <- gg2 + facet_wrap(~Patient,ncol=1)
-    gg2 <- gg2 + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank())
+    gg2 <- gg2 + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) + theme(legend.position = "none")
     breaks <- data.frame(xs = rep(c(0.5, 1:(length(seq))+0.5),nPerms))
     gg2 <- gg2 + geom_vline(data=breaks, aes(xintercept=xs)) + xlab("")
     gg2
-    
+
     grid.arrange(gg,gg2)
-      
+
   })
-  
-  output$mutationPlot <- reactivePlot(function(){
+
+  output$mutationPlot <- renderPlot({
     start <- 41196312
     length<-10
     nPerms  <- 5
     seq <- generate.sequence()[[1]]
-    
+
     useColour <- ifelse(input$useColour == "Yes",TRUE,FALSE)
     useText <- ifelse(input$useText == "Yes",TRUE,FALSE)
-    
+
     df <- data.frame(pos=1:length(seq),letter=seq,Patient="Human Genome")
     df$letter <- factor(df$letter, levels= c("A","C","G","T"))
-    
+
     if(useColour){
-      gg <- ggplot(df, aes(x=pos,y=1,fill=letter,label=letter)) +geom_tile(position="identity") 
+      gg <- ggplot(df, aes(x=pos,y=1,fill=letter,label=letter)) +geom_tile(position="identity")
     }
     else gg <- ggplot(df, aes(x=pos,y=1,label=letter)) +geom_tile(position="identity",fill="white")
-    
+
     if (useText) gg <- gg + geom_text()
-    
-    gg <- gg + scale_fill_manual(values=c("A" = as.character(mypal[3]),"C"=as.character(mypal[2]),"G"=as.character(mypal[6]),"T"=as.character(mypal[1])))
+
+    gg <- gg + scale_fill_manual(values=c("A" = as.character(mypal[3]),"C"=as.character(mypal[2]),"G"=as.character(mypal[6]),"T"=as.character(mypal[1])), drop = FALSE)
     gg <- gg + facet_wrap(~Patient)
-    gg <- gg + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) + theme(legend.position="none")
+    gg <- gg + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) + theme(legend.position = "top", legend.title = element_blank())
     breaks <- data.frame(xs = c(0.5, 1:(length(seq))+0.5))
     gg <- gg + geom_vline(data=breaks, aes(xintercept=xs)) + xlab("")
-    
+
     gg
-    
+
     df <- generate.sequence()[[2]]
     df$letter <- factor(df$letter, levels= c("A","C","G","T"))
-    
-    gg3 <- ggplot(df, aes(x=pos,y=1,fill=Mutated,label=letter)) +geom_tile() 
-    
+
+    gg3 <- ggplot(df, aes(x=pos,y=1,fill=Mutated,label=letter)) +geom_tile()
+
     if(useText) gg3 <- gg3 + geom_text()
-    
+
     gg3 <- gg3 + scale_fill_manual(values=c("TRUE" = as.character(mypal[8]),"FALSE"="white")) + ggtitle("The computer will analyse the sequence and show any mutated positions in pink")
     gg3 <- gg3 + facet_wrap(~Patient,ncol=1)
-    gg3 <- gg3 + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) + theme(legend.position="none")
+    gg3 <- gg3 + theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),panel.background =  element_blank()) + theme(legend.position = "none")
     breaks <- data.frame(xs = rep(c(0.5, 1:(length(seq))+0.5),nPerms))
     gg3 <- gg3 + geom_vline(data=breaks, aes(xintercept=xs)) + xlab("")
-    
+
     grid.arrange(gg,gg3)
-  
+
     }
   )
   output$mytable <- renderDataTable({
@@ -162,7 +150,7 @@ shinyServer(function(input, output) {
     diffList <- generate.sequence()[[4]]
     out <- data.frame(Pattern = unlist(lapply(seqList,function(x) paste(x,collapse=""))), Difference = unlist(diffList), Number = unique(df$Number))
     out
-    
+
   }
-  )  
+  )
 })
